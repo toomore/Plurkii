22e918a86bb28ec1759e7efb85e0dea1ca1d6c4a
Toomore Chiang <toomore0929@gmail.com>
2009-10-02 15:31:29 +0800

    Pre wall.

--- application.py
+++ application.py
@@ -5,7 +5,8 @@ Put all application function.
 """
 
 from google.appengine.ext import db
-import datamodel,random
+import random
+import datamodel
 
 def randuser(mod = None):
     """ random to show user. """
@@ -19,3 +20,22 @@ def randuser(mod = None):
         for i in result:
             a = a + "<a href='/?u=%s'>%s</a> " % (i.uname,i.uname)
     return a
+
+def getwall(gender = 0):
+    wall = datamodel.userplurkdata.gql("where gender = :1" ,gender)
+    count = wall.count()
+    print '111'
+    print count
+    a =''
+    for i in wall:
+        if i.avatar:
+            avatar = 'http://avatars.plurk.com/%s-big%s.jpg' % (i.key().id_or_name(),i.avatar)
+        else:
+            avatar = '/images/face-angel.png'
+        a = a + "<a href='/?u=%s'><img border='0' width='50' src='%s'></a>" % (i.uname,avatar)
+    return a
+print getwall()
+print getwall(1)
+
+
+

e0d74d907bcd6e0e70a431c4eb4d6a35399b59fd
Toomore Chiang <toomore0929@gmail.com>
2012-01-23 15:56:08 +0800

    Updated v2.

--- main_v2.py
+++ main_v2.py
@@ -14,33 +14,61 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from datamodel import userplurkdata
 
+def ckini(s):
+  try:
+    return int(s)
+  except:
+    return False
+
 class MainHandler(webapp.RequestHandler):
   def get(self):
-    q = userplurkdata.gql("WHERE uname = '%s'" % self.request.get('u'))
     p2uinmem = []
     p2u = {}
-    for i in q:
-      p2u['key'] = i.key().id_or_name()
-      p2u['uname'] = i.uname
-      p2u['fullname'] = i.fullname
-      p2u['birthday'] = i.birthday
-      p2u['location'] = i.location
-      p2u['avatar'] = i.avatar
+    tv = {}
+    if ckini(self.request.get('u')) == False:
+      q = userplurkdata.gql("WHERE uname = '%s'" % self.request.get('u'))
+      for i in q:
+        p2u['key'] = i.key().id_or_name()
+        p2u['uname'] = i.uname
+        p2u['fullname'] = i.fullname
+        p2u['birthday'] = i.birthday
+        p2u['location'] = i.location
+        p2u['avatar'] = i.avatar
+        p2uinmem.append(p2u.copy())
+    else:
+      q = userplurkdata.get_by_key_name(str(int(self.request.get('u'))))
+      p2u['key'] = q.key().id_or_name()
+      p2u['uname'] = q.uname
+      p2u['fullname'] = q.fullname
+      p2u['birthday'] = q.birthday
+      p2u['location'] = q.location
+      p2u['avatar'] = q.avatar
       p2uinmem.append(p2u.copy())
+
     op = u'序號 ID 暱稱 生日 地區 頭像數<br>'
     for i in p2uinmem:
       if i['avatar'] > 0:
         i['pics'] = ''
-        for no in range(2,i['avatar']*10,2):
-          i['pics'] += "<img src='http://avatars.plurk.com/%s-big%s.jpg'>" % (i['key'],no)
+        for no in range(2,i['avatar']+2,2):
+          i['pics'] += "<img alt='' src='http://avatars.plurk.com/%s-big%s.jpg'>" % (i['key'],no)
       else:
         i['pics'] = None
       op += '%(key)s %(uname)s %(fullname)s %(birthday)s %(location)s %(avatar)s<br>%(pics)s<br>' % i
-    self.response.out.write(op)
+    tv['op'] = op
+    tv['nick_name'] = self.request.get('u')
+    self.response.out.write(template.render('./template/h_index.htm',{'tv':tv}))
+
+class Tolist(webapp.RequestHandler):
+  def get(self):
+    a = userplurkdata.all()
+    for i in a:
+      print '123'
+      print i.key().id_or_name(),i.uname
 
 def main():
   """ Start up. """
   application = webapp.WSGIApplication([('/', MainHandler),
+                                        ('/list', Tolist),
                                         ('/.*',MainHandler)],debug=True)
   run_wsgi_app(application)
 
